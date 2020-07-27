8708263d29d382a79da391f4c71544de03c1d7ef
OleksandrYehorov <egorow.sasha@gmail.com>
2020-07-28 00:12:58 +0300

    Updates

---
+++ precache-manifest.10b4af40b736c78306141e0f99ba9087.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e06bd838e99f76539bd788d9cb7e555",
+    "url": "/fifteen-puzzle/index.html"
+  },
+  {
+    "revision": "3e4446d6d0609ad1273c",
+    "url": "/fifteen-puzzle/static/css/main.ebf363f5.chunk.css"
+  },
+  {
+    "revision": "8d475551d087b9ef2bcf",
+    "url": "/fifteen-puzzle/static/js/2.457f1295.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fifteen-puzzle/static/js/2.457f1295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e4446d6d0609ad1273c",
+    "url": "/fifteen-puzzle/static/js/main.0ec5b969.chunk.js"
+  },
+  {
+    "revision": "1d77b6536a9080f2e12f",
+    "url": "/fifteen-puzzle/static/js/runtime-main.133d7edd.js"
+  }
+]);
